eb1e38da467d4a335a2e67766a567edeb7a116fd
Lucas-Severo <lucas_severo@outlook.com.br>
2020-04-21 08:38:18 -0400

    Implementado as funções para verificar os números do fibonacci

--- modulo3/desafio/src/index.js
+++ modulo3/desafio/src/index.js
@@ -1,9 +1,16 @@
 'use strict'
 
 const fibonacci = () => {
+    const numbers = [0, 1];
+
+    while(numbers[numbers.length-1] < 350) {
+        numbers.push(numbers[numbers.length-1] + numbers[numbers.length-2]);
+    }
+    return numbers;
 }
 
 const isFibonnaci = (num) => {
+    return fibonacci().includes(num);
 }
 
 module.exports = {
