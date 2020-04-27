232a96bff2b54e4c6c52dfa22773c0585bbe9b5b
Lucas-Severo <lucas_severo@outlook.com.br>
2020-04-27 10:24:04 -0400

    Adicionado testes de unidade

--- modulo4/hora-do-codigo1/calculadora/calc.test.js
+++ modulo4/hora-do-codigo1/calculadora/calc.test.js
@@ -1,10 +1,51 @@
+const calculadora = require('./calc');
 
 describe("calculadora", () => {
     it("teste exemplo", () => {
         const num1 = 23;
         const num2 = 7;
 
-        expect(23 + 7).toBe(30)
-        expect(23 + 7).not.toBe(31)
-    })
+        expect(23 + 7).toBe(30);
+        expect(23 + 7).not.toBe(31);
+    });
+
+    describe("soma", () => {
+        it("retorna 3 quando 2 somado com 1", () => {
+            const num1 = 2;
+            const num2 = 1;
+            expect(calculadora.soma(num1, num2)).toBe(3);
+        });
+    });
+
+    describe("subtracao", () => {
+        it("retorna 1 quando 1 subtraido de 2", () => {
+            const num1 = 2;
+            const num2 = 1;
+            expect(calculadora.subtracao(num1, num2)).toBe(1);
+        });
+    });
+
+    describe("multiplicacao", () => {
+        it("retorna 6 quando 2 multiplicado por 3", () => {
+            const num1 = 2;
+            const num2 = 3;
+            expect(calculadora.multiplicacao(num1, num2)).toBe(6);
+        });
+    });
+
+    describe("divisao", () => {
+        it("retorna 2 quando 6 dividido por 3", () => {
+            const num1 = 6;
+            const num2 = 3;
+            expect(calculadora.divisao(num1, num2)).toBe(2);
+        });
+
+        it("retorna 0 se divisão por 0", () => {
+            const num1 = 6;
+            const num2 = 0;
+            expect(calculadora.divisao(num1, num2)).toBe(0);
+        });
+    });
+
+
 });
