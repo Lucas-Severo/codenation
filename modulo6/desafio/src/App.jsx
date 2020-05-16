b5fbbff0123c3ecf0b46da88466a554fc4e5fdf3
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-05-15 21:13:51 -0400

    Adicionado animação de loading ao projeto

--- modulo6/desafio/src/App.jsx
+++ modulo6/desafio/src/App.jsx
@@ -11,16 +11,19 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      contacts: []
+      contacts: [],
+      isLoading: false
     }
   }
 
   componentDidMount() {
+      this.setState({isLoading: true});
       fetch('https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts')
       .then(response => response.json())
       .then(response => {
         this.setState({
-          contacts: response
+          contacts: response,
+          isLoading: false
         });
       });
   }
@@ -31,7 +34,13 @@ class App extends React.Component {
       <div className="app" data-testid="app">
         <Topbar />
         <Filter />
-        <Contacts contacts={this.state.contacts}/>
+        { this.state.isLoading ? (
+          <div className="loading__container">
+            <div className="loading"> </div>
+          </div>
+        ) : (
+          <Contacts contacts={this.state.contacts}/>
+        )}
       </div>
     )
   }
